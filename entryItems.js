62696694d24bb3a9518e17502129a087ed2f1f07
bee0060 <be0060@126.com>
2025-12-24 22:27:00 +0800

    feat: 更新词条数据

--- entryItems.js
+++ entryItems.js
@@ -4,6 +4,87 @@ const entryItems = [
   {
     "category": "instrument",
     "subCategory": "",
+    "name": "对玩家造成伤害提升",
+    "quality": "orange",
+    "min": 2.49,
+    "max": 3.38,
+    "time": "2025/12/24 17:04:23"
+  },
+  {
+    "category": "instrument",
+    "subCategory": "",
+    "name": "对玩家造成伤害提升",
+    "quality": "red",
+    "min": 3.49,
+    "max": 4.5,
+    "time": "2025/12/24 16:58:37"
+  },
+  {
+    "category": "instrument",
+    "subCategory": "",
+    "name": "伤害加成提升",
+    "quality": "orange",
+    "min": 1.67,
+    "max": 2.25,
+    "time": "2025/12/24 16:56:12"
+  },
+  {
+    "category": "instrument",
+    "subCategory": "",
+    "name": "伤害加成提升",
+    "quality": "red",
+    "min": 2.27,
+    "max": 3,
+    "time": "2025/12/24 16:52:15"
+  },
+  {
+    "category": "instrument",
+    "subCategory": "",
+    "name": "基础攻击增加",
+    "quality": "orange",
+    "min": 3.33,
+    "max": 4.5,
+    "time": "2025/12/24 16:48:35"
+  },
+  {
+    "category": "instrument",
+    "subCategory": "",
+    "name": "基础攻击增加",
+    "quality": "red",
+    "min": 4.51,
+    "max": 6,
+    "time": "2025/12/24 16:41:23"
+  },
+  {
+    "category": "instrument",
+    "subCategory": "",
+    "name": "水属性伤害提升",
+    "quality": "red",
+    "min": 3.43,
+    "max": 4.5,
+    "time": "2025/12/24 08:00:45"
+  },
+  {
+    "category": "instrument",
+    "subCategory": "",
+    "name": "风属性伤害提升",
+    "quality": "red",
+    "min": 3.43,
+    "max": 4.5,
+    "time": "2025/12/24 08:00:41"
+  },
+  {
+    "category": "instrument",
+    "subCategory": "",
+    "name": "火属性伤害提升",
+    "quality": "red",
+    "min": 3.43,
+    "max": 4.5,
+    "time": "2025/12/24 08:00:31"
+  },
+  {
+    "category": "instrument",
+    "subCategory": "",
     "name": "异常命中提升",
     "quality": "orange",
     "min": 3.32,
@@ -43,7 +124,7 @@ const entryItems = [
     "name": "地属性伤害提升",
     "quality": "red",
     "min": 3.43,
-    "max": 4.49,
+    "max": 4.5,
     "time": "2025/12/22 22:53:49"
   },
   {
